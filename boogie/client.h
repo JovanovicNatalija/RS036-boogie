4fbd0d2d87429aa36e2d6c9eff16f7156a9e10fc
JovanovicNatalija <ninijovanovic96.nj@gmail.com>
2018-12-07 20:15:14 +0100

    Implemented basic client class

--- boogie/client.h
+++ boogie/client.h
@@ -1,11 +1,16 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-
-class client
+#include <QTcpSocket>
+//TODO refactor client->Client
+class client : public QTcpSocket
 {
+    Q_OBJECT
 public:
-    client();
+    client(QString ip, QObject* parrent = nullptr, quint16 port = 10000);
+
+public slots:
+    void readMsg();
 };
 
 #endif // CLIENT_H
