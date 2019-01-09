0f3e44592e242825a559e9bb7c8be40e9c08f5c8
Nikola Milovanovic <nikolamilovanovic94@gmail.com>
2019-01-09 20:48:42 +0100

    added chat group struct and create group message type

--- util/util.h
+++ util/util.h
@@ -4,12 +4,18 @@
 #include <QString>
 #include <QJsonObject>
 
+typedef struct{
+    QString groupName;
+    QSet members;
+    int id;
+    QSet admins;
+} chatGroup;
 
 QString packMessage(const QJsonObject& dataToSend);
 
 enum class MessageType : int {Authentication = 1, Text, Contacts, ContactLogout,
 							  ContactLogin, BadPass, AllreadyLoggedIn,
-                              BadMessageFormat, AddNewContact};
+                              BadMessageFormat, AddNewContact, CreateGroup};
 
 bool operator==(const QJsonValue &v, const MessageType& type);
 
