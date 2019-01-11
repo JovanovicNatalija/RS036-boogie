ad897aba42278a9c42a40f1c4c1d4db6c9e58066
Vuk Novakovic <vnovakovic96@gmail.com>
2019-01-11 02:30:36 +0100

    command line argument for port number is now optional

--- boogie_server/src/main.cpp
+++ boogie_server/src/main.cpp
@@ -8,12 +8,13 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
-	if(argc < 2){
-		qDebug() << "BAD CALL! add port number as argument";
-		return 1;
+	quint16 port;
+	if(argc == 2){
+		port = quint16(atoi(argv[1]));
 	}
-
-	quint16 port = quint16(atoi(argv[1]));
+	else{
+		port = 10000;
+	}
 	Server s(port);
 
 	if(!s.isInitialized()){
